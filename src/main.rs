962dcb8ebe08d37d0e9a662a6880e2ba6d9e3ed3
cjonas1999 <cjonas1999@gmail.com>
2025-08-26 19:52:36 -0500

    config detection to find controller and 3 mashing triggers to read

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,14 @@ use std::sync::{Arc, RwLock};
 use std::thread;
 use vigem_client::{Client, XButtons, XGamepad};
 
+static NUM_MASHING_KEYS: usize = 3;
+
 enum AppState {
-    DETECT_CONFIG,
-    ACCEPTING_INPUT,
+    DetectConfig,
+    AcceptingInput,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug, Clone)]
 enum VigemInput {
     Button(u16),
     LeftTrigger,
@@ -49,18 +51,18 @@ fn sdl_button_to_vigem(button: sdl3::gamepad::Button) -> Option<VigemInput> {
 }
 
 fn main() {
-    sdl3::hint::set("SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS", "1");
 
+    sdl3::hint::set("SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS", "1");
     let sdl_context = sdl3::init().unwrap();
     let gamepad_system = sdl_context.gamepad().unwrap();
-
     // TODO: find controller by id from config file
     // need to be able to update this when configuring
+    let mut opened_gamepads: Vec<sdl3::gamepad::Gamepad> = Vec::new();
     let mut current_controller = gamepad_system
         .gamepads()
         .unwrap()
         .into_iter()
-        .find_map(|j| match gamepad_system.open(j) {
+        .map(|j| match gamepad_system.open(j) {
             Ok(c) => {
                 println!("Success: opened controller \"{}\"", c.name().unwrap());
                 Some(c)
@@ -70,78 +72,241 @@ fn main() {
                 None
             }
         })
-        .expect("failed to open any joysticks");
+        .flatten()
+        .collect::<Vec<sdl3::gamepad::Gamepad>>()
+        .remove(0);
 
+    // VIGEM setup
     let client = vigem_client::Client::connect().unwrap();
     let id = vigem_client::TargetId::XBOX360_WIRED;
     let mut target = vigem_client::Xbox360Wired::new(client, id);
-    target.plugin().map_err(|e| e.to_string());
-    target.wait_ready().map_err(|e| e.to_string());
+    target.plugin().expect("Failed to plugin virtual controller");
+    target.wait_ready().expect("Could not wait for virtual controller to ready");
     let mut gamepad_state = vigem_client::XGamepad::default();
 
-    let current_app_state = AppState::ACCEPTING_INPUT;
+    // App state setup
+    // TODO: move what needs to into global state
+    let mut current_app_state = AppState::AcceptingInput;
     let mut mashing_trigger_state: Vec<MashingTrigger> = Vec::new();
     let mut should_mash = false;
     let mut config_held_buttons: HashMap<u32, Vec<VigemInput>> = HashMap::new();
+    let mut config_detect_needs_initialized = true;
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     'mainloop: loop {
-        event_pump.pump_events();
-        for event in event_pump.poll_iter() {
-            match current_app_state {
-                AppState::ACCEPTING_INPUT => {
-                    match event {
-                        Event::ControllerButtonDown { which, button, .. } => {
-                            println!("controller down {}", button.string());
-                            if which != current_controller.id().unwrap_or(u32::MAX) { continue; }
+        //===============================
+        // Accepting Input for Mashing
+        //===============================
+        if matches!(current_app_state, AppState::AcceptingInput) {
+            event_pump.pump_events();
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::ControllerButtonDown { which, button, .. } => {
+                        println!("controller down {}", button.string());
+                        if which != current_controller.id().unwrap_or(u32::MAX) { continue; }
 
-                            if let Some(vigem_button) = sdl_button_to_vigem(button) {
-                                if let Some(pressed_button) = mashing_trigger_state.iter_mut().find(|trigger| trigger.button == vigem_button) {
-                                    pressed_button.is_pressed = true;
-                                }
-                            }
-                        }
-                        Event::ControllerButtonUp { which, button, .. } => {
-                            println!("controller up {}", button.string());
-                            if which != current_controller.id().unwrap_or(u32::MAX) { continue; }
-
-                            if let Some(vigem_button) = sdl_button_to_vigem(button) {
-                                if let Some(pressed_button) = mashing_trigger_state.iter_mut().find(|trigger| trigger.button == vigem_button) {
-                                    pressed_button.is_pressed = false;
-                                }
+                        if let Some(vigem_button) = sdl_button_to_vigem(button) {
+                            if let Some(pressed_button) = mashing_trigger_state.iter_mut().find(|trigger| trigger.button == vigem_button) {
+                                pressed_button.is_pressed = true;
                             }
                         }
 
-                        Event::ControllerAxisMotion { which, axis, value, .. } => {
-                            match axis {
-                                sdl3::gamepad::Axis::TriggerLeft => {
-                                    if which != current_controller.id().unwrap_or(u32::MAX) { continue; }
+                        if button == sdl3::gamepad::Button::Start {
+                            println!("ENTERING CONFIG DETECTION MODE\n============================");
+                            current_app_state = AppState::DetectConfig;
+                            continue 'mainloop;
+                        }
+                    }
+                    Event::ControllerButtonUp { which, button, .. } => {
+                        println!("controller up {}", button.string());
+                        if which != current_controller.id().unwrap_or(u32::MAX) { continue; }
 
-                                    if let Some(pressed_trigger) = mashing_trigger_state.iter_mut().find(|m| m.button == VigemInput::LeftTrigger) {
-                                        pressed_trigger.is_pressed = value != 0;
-                                    }
-                                }
-                                sdl3::gamepad::Axis::TriggerRight => {
-                                    if which != current_controller.id().unwrap_or(u32::MAX) { continue; }
-
-                                    if let Some(pressed_trigger) = mashing_trigger_state.iter_mut().find(|m| m.button == VigemInput::RightTrigger) {
-                                        pressed_trigger.is_pressed = value != 0;
-                                    }
-                                }
-                                _ => (),
+                        if let Some(vigem_button) = sdl_button_to_vigem(button) {
+                            if let Some(pressed_button) = mashing_trigger_state.iter_mut().find(|trigger| trigger.button == vigem_button) {
+                                pressed_button.is_pressed = false;
                             }
-                        },
-
-                        Event::KeyDown { keycode: Some(sdl3::keyboard::Keycode::Escape), .. } => break 'mainloop,
-                        Event::Quit { .. } => break 'mainloop,
-                        _ => (),
+                        }
                     }
 
-                    // check if all triggers are pressed and activate the mashing
-                    should_mash = mashing_trigger_state.iter().all(|t| t.is_pressed);
+                    Event::ControllerAxisMotion { which, axis, value, .. } => {
+                        match axis {
+                            sdl3::gamepad::Axis::TriggerLeft => {
+                                if which != current_controller.id().unwrap_or(u32::MAX) { continue; }
+
+                                if let Some(pressed_trigger) = mashing_trigger_state.iter_mut().find(|m| m.button == VigemInput::LeftTrigger) {
+                                    pressed_trigger.is_pressed = value != 0;
+                                }
+                            }
+                            sdl3::gamepad::Axis::TriggerRight => {
+                                if which != current_controller.id().unwrap_or(u32::MAX) { continue; }
+
+                                if let Some(pressed_trigger) = mashing_trigger_state.iter_mut().find(|m| m.button == VigemInput::RightTrigger) {
+                                    pressed_trigger.is_pressed = value != 0;
+                                }
+                            }
+                            _ => (),
+                        }
+                    },
+
+                    Event::KeyDown { keycode: Some(sdl3::keyboard::Keycode::Escape), .. } => break 'mainloop,
+                    Event::Quit { .. } => break 'mainloop,
+                    _ => (),
                 }
 
-                AppState::DETECT_CONFIG => {}
+                // check if all triggers are pressed and activate the mashing
+                should_mash = mashing_trigger_state.iter().all(|t| t.is_pressed);
+            }
+        }
+
+
+        //========================
+        // Config Detection
+        // =======================
+        else if matches!(current_app_state, AppState::DetectConfig) {
+            let mut config_finalized = false;
+
+            if config_detect_needs_initialized {
+                config_held_buttons = HashMap::new();
+                config_detect_needs_initialized = false;
+
+                // open all gamepads
+                opened_gamepads = gamepad_system
+                    .gamepads()
+                    .unwrap()
+                    .into_iter()
+                    .map(|j| match gamepad_system.open(j) {
+                        Ok(c) => {
+                            println!("Success: opened controller \"{}\"", c.name().unwrap());
+                            Some(c)
+                        }
+                        Err(e) => {
+                            println!("failed: {:?}", e);
+                            None
+                        }
+                    })
+                    .flatten()
+                    .collect();
+            }
+
+            
+            event_pump.pump_events();
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::ControllerButtonDown { which, button, .. } => {
+                        println!("controller down {}", button.string());
+                        
+                        if let Some(input) = sdl_button_to_vigem(button) {
+                            if !config_held_buttons.contains_key(&which) {
+                                config_held_buttons.insert(which, vec![input]);
+                            } else {
+
+                                if let Some(held) = config_held_buttons.get_mut(&which) {
+                                    held.push(input);
+
+                                    if held.len() == NUM_MASHING_KEYS  {
+                                        config_finalized = true;
+                                    }
+                                }
+                            }
+                        }
+
+                        dbg!(&config_held_buttons);
+                    }
+                    Event::ControllerButtonUp { which, button, .. } => {
+                        println!("controller up {}", button.string());
+
+                        if let Some(entry) = config_held_buttons.get_mut(&which) {
+                            if let Some(input) = sdl_button_to_vigem(button) {
+                                entry.retain(|held| *held != input);
+
+                                if entry.is_empty() {
+                                    config_held_buttons.remove_entry(&which);
+                                }
+                            }
+                        }
+                        dbg!(&config_held_buttons);
+                    }
+
+                    Event::ControllerAxisMotion { which, axis, value, .. } => {
+                        match axis {
+                            sdl3::gamepad::Axis::TriggerLeft => {
+                                println!("left trigger {}", value);
+                                let input = VigemInput::LeftTrigger;
+                            
+                                if value > 0 {
+                                    if !config_held_buttons.contains_key(&which) {
+                                        config_held_buttons.insert(which, vec![input]);
+                                    } else {
+                                        if let Some(held) = config_held_buttons.get_mut(&which) {
+                                            if !held.iter().any(|x| *x == input) {
+                                                held.push(input);
+                                            }
+
+                                            if held.len() == NUM_MASHING_KEYS {
+                                                config_finalized = true;
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    if let Some(entry) = config_held_buttons.get_mut(&which) {
+                                        entry.retain(|held| *held != input);
+
+                                        if entry.is_empty() {
+                                            config_held_buttons.remove_entry(&which);
+                                        }
+                                    }
+                                }
+                            }
+                            sdl3::gamepad::Axis::TriggerRight => {
+                                println!("right trigger {}", value);
+                                let input = VigemInput::RightTrigger;
+                            
+                                if value > 0 {
+                                    if !config_held_buttons.contains_key(&which) {
+                                        config_held_buttons.insert(which, vec![input]);
+                                    } else {
+                                        if let Some(held) = config_held_buttons.get_mut(&which) {
+                                            if !held.iter().any(|x| *x == input) {
+                                                held.push(input);
+                                            }
+
+                                            if held.len() == NUM_MASHING_KEYS {
+                                                config_finalized = true;
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    if let Some(entry) = config_held_buttons.get_mut(&which) {
+                                        entry.retain(|held| *held != input);
+
+                                        if entry.is_empty() {
+                                            config_held_buttons.remove_entry(&which);
+                                        }
+                                    }
+                                }
+                            }
+                            _ => (),
+                        }
+                    },
+
+                    Event::KeyDown { keycode: Some(sdl3::keyboard::Keycode::Escape), .. } => break 'mainloop,
+                    Event::Quit { .. } => break 'mainloop,
+                    _ => (),
+                }
+
+                if config_finalized {
+                    for (k, val) in config_held_buttons.iter() {
+                        if val.len() == NUM_MASHING_KEYS {
+                            current_controller = gamepad_system.open(*k).unwrap();
+                            mashing_trigger_state = val.iter().map(|vig_in| MashingTrigger {button: vig_in.clone(), is_pressed: false}).collect();
+                            current_app_state = AppState::AcceptingInput;
+                            config_detect_needs_initialized = true;
+                            println!("ENTERING ACCEPTING INPUT MODE\n===================================");
+
+                            continue 'mainloop;
+                        }
+                    }
+                }
             }
         }
     }
